4ad2f089c11a691fb62da582f953fbafbef32aa8
Aarush336 <aarushupadhyay614@gmail.com>
2026-03-10 09:37:00 +0530

    Practise Question 1: Variables and Data Types

---
+++ DAY_3.py
@@ -0,0 +1,11 @@
+# Practise question 1: Create 4 variables representing a plant's data — its name, its age in days, its current temperature, and whether it's alive or not. Print all four. 
+
+name = "Cactus"
+age = 37
+temperature = 41
+is_it_alive = True
+
+print ("Name:- " +name)
+print("Age:- " ,age)
+print("Current temperature:- " ,temperature)
+print("Is it alive:- " ,is_it_alive)
