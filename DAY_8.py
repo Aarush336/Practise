37c49a2a020210f78b192b13066def6656bc995b
Aarush336 <aarushupadhyay614@gmail.com>
2026-03-15 23:10:22 +0530

    Made a model to predict if a plant needs water on a self-generated Dataset

---
+++ DAY_8.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+np.random.seed(42)
+
+n = 100
+
+temperature = np.random.randint(25, 50, n)
+humidity = np.random.randint(15, 80, n)
+soil_moisture = np.random.randint(10, 80, n)
+days_since_watered = np.random.randint(1, 14, n)
+
+df = pd.DataFrame({
+    "temperature" : temperature,
+    "humidity" : humidity,
+    "soil_moisture" : soil_moisture,
+    "days_since_watered" : days_since_watered
+})
+
+df["water_needed"] = np.where(
+    ((df["temperature"] > 39)
+    &
+    (df["days_since_watered"] > 3))
+    |
+    ((df["temperature"] > 39)
+    &
+    (df["soil_moisture"] < 40))
+    |
+    ((df["humidity"] < 40)
+    &
+    (df["soil_moisture"] < 40)),
+    "Yes", "No"
+)
+
+print(df.head(10))
+print(df["water_needed"].value_counts())
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+X = df[["temperature", "humidity", "soil_moisture", 'days_since_watered']]
+Y = df["water_needed"]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, Y_train)
+
+print("Model trained succesfully")
+print("Training rows:", len(X_train))
+print("Testing row:", len(X_test))
+
+from sklearn.metrics import accuracy_score
+prediction = model.predict(X_test)
+accuracy = accuracy_score(Y_test, prediction)
+print("Accuracy:", accuracy)
